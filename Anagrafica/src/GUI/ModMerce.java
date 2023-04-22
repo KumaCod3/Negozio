8768dcce0c59d89e60c22872a1fb1fd6de696251
mixim <mixim@pCla.fritz.box>
2023-04-22 11:04:24 +0200

    added classes for DB

---
+++ Anagrafica/src/GUI/ModMerce.java
@@ -0,0 +1,149 @@
+package GUI;
+import Negozio.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.sql.SQLException;
+
+public class ModMerce extends Finestra{
+		String nome="nome";
+		Double quantita=0.0;
+		Double prezzoA=0.0;
+		int rincaro=0;
+		String unita="unita";
+		
+		public ModMerce(int x){
+			super("Product");
+			
+			JPanel contenuto=new JPanel();
+			contenuto.setBorder(Est.bordo);
+			contenuto.setOpaque(false);
+			contenuto.setLayout(new GridLayout(6,1));
+			
+			JPanel panel_1 = new JPanel();
+			FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
+			flowLayout.setAlignment(FlowLayout.TRAILING);
+			flowLayout.setAlignOnBaseline(true);
+			panel_1.setOpaque(false);
+			contenuto.add(panel_1);
+			
+			Etichetta etNome = new Etichetta("Product:             ");
+			panel_1.add(etNome);
+			
+			FormVuoto tf1 = new FormVuoto(nome);
+			panel_1.add(tf1);
+			
+			JPanel panel_2 = new JPanel();
+			FlowLayout flowLayout_1 = (FlowLayout) panel_2.getLayout();
+			flowLayout_1.setAlignment(FlowLayout.TRAILING);
+			panel_2.setOpaque(false);
+			contenuto.add(panel_2);
+			
+			Etichetta etUni = new Etichetta("Unit of measure:              ");
+			panel_2.add(etUni);
+			
+			Etichetta uni = new Etichetta("             "+unita);
+			panel_2.add(uni);
+			
+			JPanel panel_3 = new JPanel();
+			FlowLayout flowLayout_2 = (FlowLayout) panel_3.getLayout();
+			flowLayout_2.setAlignment(FlowLayout.TRAILING);
+			panel_3.setOpaque(false);
+			contenuto.add(panel_3);
+			
+			Etichetta etQuant = new Etichetta("Quantity:            ");
+			panel_3.add(etQuant);
+			
+			FormVuoto tf2 = new FormVuoto(""+quantita);
+			panel_3.add(tf2);
+			
+			JPanel panel_4 = new JPanel();
+			FlowLayout flowLayout_3 = (FlowLayout) panel_4.getLayout();
+			flowLayout_3.setAlignment(FlowLayout.TRAILING);
+			panel_4.setOpaque(false);
+			contenuto.add(panel_4);
+			
+			Etichetta etPri  = new Etichetta("Purchase price:            ");
+			panel_4.add(etPri);
+			
+			FormVuoto tf3 = new FormVuoto(prezzoA+"");
+			panel_4.add(tf3);
+			
+			JPanel panel_5 = new JPanel();
+			FlowLayout flowLayout_5 = (FlowLayout) panel_5.getLayout();
+			flowLayout_5.setAlignment(FlowLayout.TRAILING);
+			panel_5.setOpaque(false);
+			contenuto.add(panel_5);
+			
+			Etichetta etRinc = new Etichetta("Choose %price increase:              ");
+			panel_5.add(etRinc);
+			
+			Choice rim = new Choice();
+			rim.setPreferredSize(Est.choi);
+			rim.add(""+rincaro);
+			rim.add("10");
+			rim.add("20");
+			rim.add("30");
+			rim.add("50");
+			rim.setFont(Est.plainFont);
+			rim.addFocusListener(new FocusListener() {
+				public void focusGained(FocusEvent e){
+				}
+				public void focusLost(FocusEvent e){
+					if (rim.getSelectedIndex()>0){
+						rincaro=Integer.parseInt(rim.getSelectedItem());
+					}
+				}
+			});
+			panel_5.add(rim);
+			
+			JPanel panel_6 = new JPanel();
+			panel_6.setOpaque(false);
+			contenuto.add(panel_6);
+			
+			Bottone bex = new Bottone("Back");
+			bex.but.addActionListener(new ActionListener() {
+			    public void actionPerformed(ActionEvent e) {
+			    	ConsultaMerci consultaM=new ConsultaMerci();
+			    	consultaM.setVisible(true);
+			    	setVisible(false);
+			    	dispose();
+				}
+			});
+			
+			panel_6.add(bex);
+			
+			Bottone bent = new Bottone("ENTER");
+			bent.but.addActionListener(new ActionListener() {
+			    public void actionPerformed(ActionEvent e) {
+
+				    	nome=tf1.ret;
+				    	try {
+				    		quantita=Double.parseDouble(tf2.ret);
+				    		prezzoA=Double.parseDouble(tf3.ret);
+//				    		Merce inserisci =new Merce(nome, quantita, rincaro, prezzoA, unita);
+//							DataM.agg(inserisci);
+				    		
+				    		try {
+					    		String dati=nome+unita+quantita+prezzoA+rincaro+"note";
+					    		Main.db.aggMerc(dati);
+				    		} catch (SQLException ex) {	ex.printStackTrace(); }
+				    		
+							ConsultaMerci consultaM=new ConsultaMerci(/*c*/);
+					    	consultaM.setVisible(true);
+//					    	MyReadM.scarica();
+					    	setVisible(false);
+					    	dispose();
+				    	}
+				    	catch (Exception ex){
+				    		Errore err=new Errore("Enter number with . ");
+				    		err.setVisible(true);
+				    	}
+				}
+			});
+			panel_6.add(bent);
+			
+			c.add("Center", contenuto);
+			pack();
+		}
+	}
---
+++ Anagrafica/src/Negozio/myDB.java
@@ -0,0 +1,201 @@
+package Negozio;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class myDB {
+	public static Connection connection;
+	public static Statement statement;
+	
+	public myDB() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); 
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/negozioDB?characterEncoding=latin1&useConfigs=maxPerformance","root","Du1k3rKnows!");
+			statement = connection.createStatement();
+			System.out.println("DB connected!");
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public String leggiCliID(int x) throws SQLException{
+		String sql="SELECT * FROM Clienti WHERE ID_CLIENTE="+x;
+		ResultSet result = statement.executeQuery(sql);
+		String inser="";
+			
+		while (result.next()) {
+			int id = result.getInt("ID_Cliente");
+			String nome=result.getString("name");
+			String cognome=result.getString("last_name");
+			String telefono=result.getString("phone");
+			String email=result.getString("Mail");
+			String iva=result.getString("vatn");
+			String stato=result.getString("state");
+			String citta=result.getString("city");
+			String indirizzo=result.getString("street");
+			Double saldo=result.getDouble("tot_sold");
+			String note=result.getString("note");
+			System.out.println(id+ nome+ cognome+ telefono+ email+ iva+ indirizzo+ saldo);
+			inser=id+","+nome+","+cognome+","+telefono+","+email+","+stato+","+citta+","+indirizzo+","+iva+","+saldo+","+note;
+		}
+		return inser;
+	}
+	public String leggiForID(int x) throws SQLException{
+		String sql="SELECT * FROM Fornitori WHERE ID_FORNITORE="+x;
+		ResultSet result = statement.executeQuery(sql);
+		String inser="";
+			
+		while (result.next()) {
+			int id = result.getInt("ID_FORNITORE");
+			String nome=result.getString("name");
+			String cognome=result.getString("last_name");
+			String telefono=result.getString("phone");
+			String email=result.getString("Mail");
+			String iva=result.getString("vatn");
+			String stato=result.getString("state");
+			String citta=result.getString("city");
+			String indirizzo=result.getString("street");
+			Double saldo=result.getDouble("tot_purchased");
+			String note=result.getString("note");
+			System.out.println(id+ nome+ cognome+ telefono+ email+ iva+ indirizzo+ saldo);
+			inser=id+","+nome+","+cognome+","+telefono+","+email+","+stato+","+citta+","+indirizzo+","+iva+","+saldo+","+note;
+		}
+		return inser;
+	}
+	public String leggiMercID(int x) throws SQLException{
+		String sql="SELECT * FROM Merci WHERE ID_MERCE="+x;
+		ResultSet result = statement.executeQuery(sql);
+		String inser="";
+			
+		while (result.next()) {
+			int id = result.getInt("ID_Merce");
+			String nome=result.getString("Product");
+			String unita=result.getString("Unity");
+			Double quantita=result.getDouble("Quantity");
+			Double prezzoA=result.getDouble("Price");
+			Double rincaro=result.getDouble("deal");
+			String note=result.getString("note");
+			System.out.println(id+ nome+ unita+ quantita+ prezzoA+ rincaro+ note);
+			inser=id+","+nome+","+unita+","+quantita+","+prezzoA+","+rincaro+","+note;
+		}
+		return inser;
+	}
+	
+	public void aggCli(String x) throws SQLException{
+		String sql="INSERT INTO Clienti (name, last_name, phone, Mail, state, city, street, vatn, tot_sold, note) values('"+x+"')";
+		int result = statement.executeUpdate(sql);
+		if (result!=0) {
+			System.out.println("Suxcesfully added!");
+		}
+	}
+	public void aggFor(String x) throws SQLException{
+		String sql="INSERT INTO Fornitori (name, last_name, phone, Mail, state, city, street, vatn, tot_purchased, note) values('"+x+"')";
+		int result = statement.executeUpdate(sql);
+		if (result!=0) {
+			System.out.println("Suxcesfully added!");
+		}
+	}
+	public void aggMerc(String x) throws SQLException{
+		String sql="INSERT INTO Merci (product,unity,quantity,price,deal,note) values("+x+")";
+		int result = statement.executeUpdate(sql);
+		if (result!=0) {
+			System.out.println("Suxcesfully added!");
+		}
+	}
+	
+	public void modCliID(int x,String nome,String cognome,String telefono,String email,String stato,String citta,String indirizzo,String iva,Double saldo,String note) throws SQLException{
+		String mods="name='"+nome+"' last_name='"+cognome+"' phone='"+telefono+"' mail='"+email+"' state='"+stato+"' city='"+citta+
+				"' street='"+indirizzo+"' vatn='"+iva+"' tot_sold='"+saldo+"' note='"+note;
+		
+		String sql="UPDATE Clienti SET "+mods+" WHERE ID_CLIENTE="+x;
+		int result = statement.executeUpdate(sql);
+		
+		if (result!=0) {
+			System.out.println("Suxcesfully added!");
+		}
+	}
+	public void modForID(int x,String nome,String cognome,String telefono,String email,String stato,String citta,String indirizzo,String iva,Double saldo,String note) throws SQLException{
+		String mods="name='"+nome+"' last_name='"+cognome+"' phone='"+telefono+"' mail='"+email+"' state='"+stato+"' city='"+citta+
+				"' street='"+indirizzo+"' vatn='"+iva+"' tot_purchased='"+saldo+"' note='"+note;
+		
+		String sql="UPDATE Fornitori SET "+mods+" WHERE ID_FORNITORE="+x;
+		int result = statement.executeUpdate(sql);
+		
+		if (result!=0) {
+			System.out.println("Suxcesfully added!");
+		}
+	}
+	public void modMercID(int x,String nome,String unita,Double quantita,Double prezzoA,Double rincaro,String note) throws SQLException{
+		String mods="product='"+nome+"' unity='"+unita+"' quantity='"+quantita+"' price='"+prezzoA+"' deal='"+rincaro+"' note='"+note;
+		
+		String sql="UPDATE Merci SET "+mods+" WHERE ID_MERCE="+x;
+		int result = statement.executeUpdate(sql);
+		
+		if (result!=0) {
+			System.out.println("Suxcesfully added!");
+		}
+	}
+	
+	public String getForName(int x) throws SQLException {
+		String sql="SELECT name FROM Fornitori WHERE ID_FORNITORE="+x;
+		ResultSet result = statement.executeQuery(sql);
+		result.next();
+		String inser=result.getString(1);
+		
+		return inser;
+	}
+	public String getMerName(int x) throws SQLException {
+		String sql="SELECT product FROM Merci WHERE ID_MERCE="+x;
+		ResultSet result = statement.executeQuery(sql);
+		String inser="";
+		try {
+			result.next();
+			inser=result.getString(0);
+		} catch (SQLException ex) { return null; }
+		
+		return inser;
+	}
+
+	public ResultSet getElenForn() throws SQLException {
+		String sql="SELECT ID_FORNITORE,name,last_name FROM fornitori";
+		ResultSet result = statement.executeQuery(sql);
+		return result;
+	}
+	public ResultSet getElenCli() throws SQLException {
+		String sql="SELECT ID_CLIENTE,name,last_name FROM clienti";
+		ResultSet result = statement.executeQuery(sql);
+		return result;
+	}
+	public ResultSet getElenMerc() throws SQLException {
+		String sql="SELECT ID_MERCE,product FROM merci";
+		ResultSet result = statement.executeQuery(sql);
+		return result;
+	}
+	
+	
+	public void assMerc(int codice, int index, Double cost) throws SQLException {
+		String sql="INSERT INTO forniture VALUES("+codice+", "+index+", "+cost+")";
+		int result = statement.executeUpdate(sql);
+		if (result!=0) {
+			System.out.println("Suxcesfully added!");
+		}
+	}
+
+	public ResultSet getElenSuppF(int index) throws SQLException{
+		String sql="SELECT forniture.ID_FORNITORE, fornitori.name, fornitori.last_name FROM forniture JOIN fornitori ON ID_FORNITORE WHERE ID_MERCE="+index;
+		// cod, nome, cogn
+		ResultSet result = statement.executeQuery(sql);
+		return result;
+	}
+
+	public ResultSet getElenSuppM(int codice) throws SQLException{
+		String sql="SELECT forniture.ID_MERCE, merci.product FROM forniture JOIN merci ON forniture.ID_MERCE=merci.ID_MERCE WHERE ID_FORNITORE="+codice;
+		// num. prod
+		ResultSet result = statement.executeQuery(sql);
+		return result;
+	}
+}
