ffa6ca8d93096bb8d2d70866d5ca7cfb9617594c
KumaCod3 <kumacod3@proton.me>
2023-08-24 10:12:58 +0200

    fin

--- Anagrafica/src/GUI/Main.java
+++ Anagrafica/src/GUI/Main.java
@@ -8,7 +8,6 @@ public class Main{
 	public static void main(String[] args) {
 		DBhandler dd=new DBhandler();
 		do {
-			
 		} while (dd.ready);
 		Home prova=new Home();
 		prova.setVisible(true);
